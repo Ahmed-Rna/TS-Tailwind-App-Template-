4b0f8ad11fb197a940a806054eec534471be1a3c
Ahmed <aw2578527@gmail.com>
2025-10-23 09:34:46 +0500

    updated to expo 54

--- App.tsx
+++ App.tsx
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import tw from 'twrnc';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      {/* <Text>Open up App.tsx to start working on your app!</Text>
+       */}
+      <Text style={tw`text-blue-500 text-lg font-bold`}>Hello, Tailwind CSS in React Native!</Text>
       <StatusBar style="auto" />
     </View>
   );
